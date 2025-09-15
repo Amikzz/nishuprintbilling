8a3443e760848b470d302aedbe9fcfcfd9e48819
Amika Subasinghe <amixx2005@gmail.com>
2025-09-15 09:31:15 +0530

    Add docblock to base controller and type hints to CustomerController
    Added a detailed docblock to the abstract Controller class to clarify its purpose and usage. Updated all public methods in CustomerController to include void return type hints for improved type safety and code clarity.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,29 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * --------------------------------------------------------------------------
+ * Base Controller
+ * --------------------------------------------------------------------------
+ *
+ * This is the abstract base class for all controllers in your Laravel app.
+ * By default, it doesn’t contain any logic, but every controller you create
+ * (e.g. `SampleInquiryController`, `UserController`, etc.) will extend this
+ * class.
+ *
+ * Why abstract?
+ * - Since this class is not meant to be instantiated directly, it is declared
+ *   as `abstract`. You only ever extend it.
+ *
+ * Typical usage:
+ * - You can place common methods here that you want to share across
+ *   multiple controllers.
+ * - Laravel’s traits (like `AuthorizesRequests`, `DispatchesJobs`,
+ *   `ValidatesRequests`) are often pulled into this class for convenience.
+ * - Example: if you add a `successResponse()` helper here, all your
+ *   controllers can use it.
+ * - This keeps your code DRY (Don’t Repeat Yourself) and organized.
+ */
 abstract class Controller
 {
     //
--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -11,7 +11,7 @@ class CustomerController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): void
     {
         //
     }
@@ -19,7 +19,7 @@ class CustomerController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): void
     {
         //
     }
@@ -27,7 +27,7 @@ class CustomerController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreCustomerRequest $request)
+    public function store(StoreCustomerRequest $request): void
     {
         //
     }
@@ -35,7 +35,7 @@ class CustomerController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Customer $customer)
+    public function show(Customer $customer): void
     {
         //
     }
@@ -43,7 +43,7 @@ class CustomerController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Customer $customer)
+    public function edit(Customer $customer): void
     {
         //
     }
@@ -51,7 +51,7 @@ class CustomerController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateCustomerRequest $request, Customer $customer)
+    public function update(UpdateCustomerRequest $request, Customer $customer): void
     {
         //
     }
@@ -59,7 +59,7 @@ class CustomerController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Customer $customer)
+    public function destroy(Customer $customer): void
     {
         //
     }
